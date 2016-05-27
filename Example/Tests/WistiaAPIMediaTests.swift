ba7da755600fb9a86fafb39087498912661b09d7
spinosa <spinosa@gmail.com>
2016-05-26 22:14:41 -0400

    Clean API media tests

--- Example/Tests/WistiaAPIMediaTests.swift
+++ Example/Tests/WistiaAPIMediaTests.swift
@@ -13,38 +13,6 @@ class WistiaAPIMediaTests: XCTestCase {
     
     let wAPI = WistiaAPI(apiToken:"1511ac67c0213610d9370ef12349c6ac828a18f6405154207b44f3a7e3a29e93")
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-
-    //MARK: - WistiaAccount
-
-    //MARK: Show
-    func testShowAccount() {
-        let expectation = expectationWithDescription("show account")
-
-        wAPI.showAccount { (account) in
-            if let a = account {
-                if a.accountID == 445830 &&
-                    a.name == "WistiaKitAutomatedTests" &&
-                    a.accountURLString == "http://wistiakitautomatedtests.wistia.com" &&
-                    a.accountURL == NSURL(string:"http://wistiakitautomatedtests.wistia.com") {
-                    expectation.fulfill()
-                }
-            }
-        }
-
-        waitForExpectationsWithTimeout(3, handler: nil)
-    }
-
-
-
     //MARK: - WistiaMedia
 
     //MARK: List
@@ -54,7 +22,7 @@ class WistiaAPIMediaTests: XCTestCase {
 
         wAPI.listMediasGroupedByProject { (projects) in
             if projects.count > 0 {
-                if let media = projects.first?.medias {
+                if let medias = projects.first?.medias where medias.count > 0 {
                     //TODO: CHECK MEDIA DETAILS
                     expectation.fulfill()
                 }
