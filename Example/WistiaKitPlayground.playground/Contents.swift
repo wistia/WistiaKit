846ed2597e9b48b8a1e726c070cc719a701706d6
Daniel Spinosa <spinosa@gmail.com>
2017-10-31 11:49:31 -0400

    Update playground for Project

--- Example/WistiaKitPlayground.playground/Contents.swift
+++ Example/WistiaKitPlayground.playground/Contents.swift
@@ -13,12 +13,42 @@ WistiaClient.default = WistiaClient(token: "COPY_TOKEN_FROM_ACCOUNT_SETTINGS")
 let projectId = "HASHED_ID_OF_A_PROJECT_WITH_MEDIA_IN_IT"
 let mediaId = "HASHED_ID_OF_A_MEDIA"
 
+Project.list { (projects, error) in
+    if let projects = projects {
+        for project in projects {
+            Media.list(projectID: project.id!, { (medias, error) in
+                if let medias = medias {
+                    print("Project [\(project.id!)] contains \(project.attributes?.videoCount ?? 99) medias:")
+                    for media in medias {
+                        print("  media: \(media.id!)")
+                    }
+                }
+                else if let error = error {
+                    print("Project [\(project.id!)] had an error reading media: \(error)")
+                }
+            })
+
+        }
+    }
+    else if let error = error {
+        print("Error reading project: \(error)")
+    }
+}
+
 Media.list(projectID: projectId) { (medias, error) in
-    print("Got medias: \(String(describing: medias))")
+    if let medias = medias {
+        print("Listing medias: \(String(describing: medias))")
+    } else {
+        print("Error listing medias: \(error!)")
+    }
 }
 
 Media(id: mediaId).show { (media, error) in
-    print("Got one media: \(String(describing: media))")
+    if let media = media {
+        print("Showing one media: \(String(describing: media))")
+    } else {
+        print("Error showing media: \(error!)")
+    }
 }
 
 
